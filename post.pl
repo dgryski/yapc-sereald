477763a45fdf8ea241a5711379bc254ad59d97fe
Damian Gryski <damian@gryski.com>
2013-07-26 17:17:09 +0200

    update perl script to use deployed version

--- post.pl
+++ post.pl
@@ -4,8 +4,9 @@ use MIME::Base64;
 use Sereal::Encoder qw(encode_sereal);
 use LWP::UserAgent;
 my $ua = LWP::UserAgent->new();
+$ua->env_proxy(1);
 
 my $data = { h => [1,2,3,4] };
 my $e = encode_base64(encode_sereal($data));
-my $r = $ua->post('http://localhost:8080/', { sereal => $e } );
+my $r = $ua->post('http://yapc-sereald.herokuapp.com/', { sereal => $e } );
 print $r->decoded_content;
